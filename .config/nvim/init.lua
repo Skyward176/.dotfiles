c4a3cebc81b35951887dc4ebb888c3c66cafd117
Brandon Martinez <brandonam04@outlook.com>
2023-04-09 09:24:37 -0400

    Fix keybind for file explorer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -169,7 +169,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-vim.keymap.set('n', '<leader>fe', ':Telescope file_browser', { noremap = true })
+vim.keymap.set('n', '<leader>fe', ':Telescope file_browser<cr>', { noremap = true })
 
 --startify
 vim.g.startify_files_number = 18
